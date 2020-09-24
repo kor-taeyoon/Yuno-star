45d3c7ee318b590aa3b021dbfa98b2634af3fe99
tae <52745064+kor-taeyoon@users.noreply.github.com>
2020-09-25 00:45:06 +0900

    Update Yuno_mount.ino
    PC프로토콜 호환이 확인.(LX200)

--- Yuno_mount.ino
+++ Yuno_mount.ino
@@ -1,139 +1,49 @@
-/*
-    Name:       yuno_star_rev1.1.ino
-    Created:	2020-07-25
-    Author:     tae
-*/
+unsigned long Period; 
+String str = "";
+void setup() {
+    Serial.begin(9600); 
+    Serial2.begin(9600);    // 4번핀에서 펄스 발생을 위해 핀모드 셋팅
 
-/*command
-0,1,2,3,4,5,6,7 -> 2^n microstepping change
-i -> in
-o -> out
-*/
-#include <SoftwareSerial.h>
-#include <SimpleTimer.h>
-#include <AccelStepper.h>
-
-//pin out
-const int en_pin = 0;
-const int ms0_pin = 0;
-const int ms1_pin = 0;
-const int ms2_pin = 0;
-const int step_pin = 0;
-const int dir_pin = 0;
-const int bluetooth_tx = 2;
-const int bluetooth_rx = 3;
-
-//object
-SoftwareSerial bluetooth_Serial(bluetooth_tx, bluetooth_rx);
-SimpleTimer timer1;
-AccelStepper focuser(1, step_pin, dir_pin);
-//first input var 1, means it connected with driver.
-
-//global variable
-char command = 0;
-short current_gear = 4;//default
-bool CanGo = false;
-
-
-void command_refresher();
-void micro_changer(int gear);
-//Main block
-void setup(){
-	bluetooth_Serial.begin(9600);
-	Serial.begin(9600);
-	pinMode(en_pin, OUTPUT);
-	pinMode(ms0_pin, OUTPUT);
-	pinMode(ms1_pin, OUTPUT);
-	pinMode(ms2_pin, OUTPUT);
-	pinMode(step_pin, OUTPUT);
-	pinMode(dir_pin, OUTPUT);
-	
-	focuser.setMaxSpeed(1000);
-	focuser.setSpeed(500);
-	
-	timer1.setInterval(command_refresher, 50);//20hz
-	
-	digitalWrite(en_pin, HIGH);
-	digitalWrite(dir_pin, LOW);
-	micro_changer(4);
-}//initializing end
-
-void loop(){
-	timer1.run();
-	if(CanGo)focuser.runSpeed();
-}//loop end
-
-
-//==================================================
-//==================== Functions ===================
-//==================================================
-void command_refresher(){
-	//refresh
-	if(bluetooth_Serial.available()){
-		command = bluetooth_Serial.read();
-	} else{
-		command = 0;
-	}
-	
-	//processing
-	if(48<=command && command <= 55){
-		micro_changer(1<<(command-48));
-		//command -> 2^(command) micro-stepping.
-	} else if(command == 'i'){
-		CanGo = true;
-		digitalWrite(en_pin, LOW);
-		digitalWrite(dir_pin, LOW);
-	} else if(command == 'o'){
-		CanGo = true;
-		digitalWrite(en_pin, LOW);
-		digitalWrite(dir_pin, HIGH);
-	} else if(command == 'e'){
-		CanGo = false;
-		digitalWrite(en_pin, HIGH);
-	}
+    Period = 21541 ;         // 16 비트 타이머니까 괄호 안의 값의 단위는 마이크로초입니다. 즉 1000000 일때 1초 주기로 타이머가 실행된답니다.
+                              // 하루 항성일주는 86164 초입니다. 마이크로초로 바꾸자면 86164 x 1000000 마이크로초
+                              // 만일 적도의의 총감속비가 마이크로스텝 포함하여 4,000,000 라고 한다면 적경축이 1 바퀴 돌기위해 스텝 모터 드라이버에 4,000,000 번의 펄스신호가 전달되어야 합니다.
+                              // 펄스간 주기는  86164 x 1000000 / 4000000 = 21541 마이크로초라고 계산됩니다. 
 }
 
-void micro_changer(int gear){
-	switch(gear){
-		case 1:
-			digitalWrite(ms0_pin, LOW);
-			digitalWrite(ms1_pin, LOW);
-			digitalWrite(ms2_pin, LOW);
-			break;
-		case 2:
-			digitalWrite(ms0_pin, HIGH);
-			digitalWrite(ms1_pin, LOW);
-			digitalWrite(ms2_pin, LOW);
-			break;
-		case 4:
-			digitalWrite(ms0_pin, LOW);
-			digitalWrite(ms1_pin, HIGH);
-			digitalWrite(ms2_pin, LOW);
-			break;
-		case 8:
-			digitalWrite(ms0_pin, HIGH);
-			digitalWrite(ms1_pin, HIGH);
-			digitalWrite(ms2_pin, LOW);
-			break;
-		case 16:
-			digitalWrite(ms0_pin, LOW);
-			digitalWrite(ms1_pin, LOW);
-			digitalWrite(ms2_pin, HIGH);
-			break;
-		case 32:
-			digitalWrite(ms0_pin, HIGH);
-			digitalWrite(ms1_pin, LOW);
-			digitalWrite(ms2_pin, HIGH);
-			break;
-		case 64:
-			digitalWrite(ms0_pin, LOW);
-			digitalWrite(ms1_pin, HIGH);
-			digitalWrite(ms2_pin, HIGH);
-			break;
-		case 128:
-			digitalWrite(ms0_pin, HIGH);
-			digitalWrite(ms1_pin, HIGH);
-			digitalWrite(ms2_pin, HIGH);
-			break;
-	}
+void loop() {
+    char c; 
+    bool isColon=false;   // ':' 콜론부터 문자를 읽기위한 변수
+    
+    while( Serial2.available() ){
+        c=Serial2.read();
+        if(c==':') isColon=true;
+        if( isColon && c!=' ' && c!='\n' && c!='\r' ) str.concat(c);
+        delay(10);
+        if(c=='#') break;
+    }
+
+    
+    char STR[30]={0}; 
+    str.toCharArray(STR,str.length()+1);
+
+    Serial.print(str);
+    if ( str==":GR#") {
+        char LX[10];
+        long VAL = 3600;
+        sprintf(LX,"%02d:%02d:%02d#",VAL/3600,(VAL%3600)/60,VAL%60);
+        
+        Serial.print(LX); 
+        Serial2.print(LX);
+    }      // VAL 은 적경위치인데 단위가 시간입니다. 제가 임의의 값 3600 으로 했어요.. 한바퀴에 86400 이니 1시입니다.
+    else if ( str==":GD#") {
+        char LX[11], s_mark;
+        long VAL = 36000;
+        if(VAL>=0) s_mark='+';
+        else s_mark='-';
+        sprintf(LX, "%c%02d*%02d:%02d#", s_mark,VAL/3600,(VAL%3600)/60,VAL%60);
+        
+        Serial.print(LX); 
+        Serial2.print(LX);
+    } // 여기서 VAL 은 적위... 80도로 했어요
+    str = "";           
 }
