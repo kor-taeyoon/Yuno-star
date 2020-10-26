2fa8287a7726474ca5e808a4d510e88d69cba783
kimtaeyoon <dbsl0512@gmail.com>
2020-10-26 22:10:04 +0900

    Update and rename move.ino to StepperController.ino

---
+++ Yuno_mount/StepperController.ino
@@ -0,0 +1,98 @@
+void TrackingPulse(){
+    //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));//for develop
+    digitalWrite(PA12, HIGH);
+    digitalWrite(PA12, LOW);
+}
+
+void GuidePulse(){
+    if(digitalRead(PB6)){
+        digitalWrite(PA12, HIGH);
+        digitalWrite(PA12, LOW);
+    }
+    if(digitalRead(PB7)){
+        digitalWrite(PA12, HIGH);
+        digitalWrite(PA12, LOW);
+    }
+    if(digitalRead(PB8)){
+        digitalWrite(PA12, HIGH);
+        digitalWrite(PA12, LOW);
+    }
+    if(digitalRead(PB9)){
+        digitalWrite(PA12, HIGH);
+        digitalWrite(PA12, LOW);
+    }
+}
+
+void MeridianFlip(){
+    //DEC goes CCW 180DEG
+    //RA goes 
+}
+
+void MicroStepChanger(int dan){
+    switch(dan){
+        case 0:
+            digitalWrite(PB4, LOW);
+            digitalWrite(PB3, LOW);
+            digitalWrite(PA15, LOW);
+            digitalWrite(PA8, LOW);
+            digitalWrite(PB15, LOW);
+            digitalWrite(PB14, LOW);
+            break;
+        case 1:
+            digitalWrite(PB4, HIGH);
+            digitalWrite(PB3, LOW);
+            digitalWrite(PA15, LOW);
+            digitalWrite(PA8, HIGH);
+            digitalWrite(PB15, LOW);
+            digitalWrite(PB14, LOW);
+            break;
+        case 2:
+            digitalWrite(PB4, LOW);
+            digitalWrite(PB3, HIGH);
+            digitalWrite(PA15, LOW);
+            digitalWrite(PA8, LOW);
+            digitalWrite(PB15, HIGH);
+            digitalWrite(PB14, LOW);
+            break;
+        case 3:
+            digitalWrite(PB4, HIGH);
+            digitalWrite(PB3, HIGH);
+            digitalWrite(PA15, LOW);
+            digitalWrite(PA8, HIGH);
+            digitalWrite(PB15, HIGH);
+            digitalWrite(PB14, LOW);
+            break;
+        case 4:
+            digitalWrite(PB4, LOW);
+            digitalWrite(PB3, LOW);
+            digitalWrite(PA15, HIGH);
+            digitalWrite(PA8, LOW);
+            digitalWrite(PB15, LOW);
+            digitalWrite(PB14, HIGH);
+            break;
+        case 5:
+            digitalWrite(PB4, HIGH);
+            digitalWrite(PB3, LOW);
+            digitalWrite(PA15, HIGH);
+            digitalWrite(PA8, HIGH);
+            digitalWrite(PB15, LOW);
+            digitalWrite(PB14, HIGH);
+            break;
+        case 6:
+            digitalWrite(PB4, LOW);
+            digitalWrite(PB3, HIGH);
+            digitalWrite(PA15, HIGH);
+            digitalWrite(PA8, LOW);
+            digitalWrite(PB15, HIGH);
+            digitalWrite(PB14, HIGH);
+            break;
+        case 7:
+            digitalWrite(PB4, HIGH);
+            digitalWrite(PB3, HIGH);
+            digitalWrite(PA15, HIGH);
+            digitalWrite(PA8, HIGH);
+            digitalWrite(PB15, HIGH);
+            digitalWrite(PB14, HIGH);
+            break;
+    }
+}
