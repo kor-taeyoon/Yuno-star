737ae42dc5a5623bebd3ed731144fedec648b5e4
kimtaeyoon <dbsl0512@gmail.com>
2020-10-26 15:55:08 +0900

    Update move.ino

--- Yuno_mount/move.ino
+++ Yuno_mount/move.ino
@@ -26,3 +26,72 @@ void guide(){
 void flip(){
     
 }
+
+void change_micro(int dan){
+    switch(dan){
+    case 0:
+        digitalWrite(PB4, LOW);
+        digitalWrite(PB3, LOW);
+        digitalWrite(PA15, LOW);
+        digitalWrite(PA8, LOW);
+        digitalWrite(PB15, LOW);
+        digitalWrite(PB14, LOW);
+        break;
+    case 1:
+        digitalWrite(PB4, HIGH);
+        digitalWrite(PB3, LOW);
+        digitalWrite(PA15, LOW);
+        digitalWrite(PA8, HIGH);
+        digitalWrite(PB15, LOW);
+        digitalWrite(PB14, LOW);
+        break;
+    case 2:
+        digitalWrite(PB4, LOW);
+        digitalWrite(PB3, HIGH);
+        digitalWrite(PA15, LOW);
+        digitalWrite(PA8, LOW);
+        digitalWrite(PB15, HIGH);
+        digitalWrite(PB14, LOW);
+        break;
+    case 3:
+        digitalWrite(PB4, HIGH);
+        digitalWrite(PB3, HIGH);
+        digitalWrite(PA15, LOW);
+        digitalWrite(PA8, HIGH);
+        digitalWrite(PB15, HIGH);
+        digitalWrite(PB14, LOW);
+        break;
+    case 4:
+        digitalWrite(PB4, LOW);
+        digitalWrite(PB3, LOW);
+        digitalWrite(PA15, HIGH);
+        digitalWrite(PA8, LOW);
+        digitalWrite(PB15, LOW);
+        digitalWrite(PB14, HIGH);
+        break;
+    case 5:
+        digitalWrite(PB4, HIGH);
+        digitalWrite(PB3, LOW);
+        digitalWrite(PA15, HIGH);
+        digitalWrite(PA8, HIGH);
+        digitalWrite(PB15, LOW);
+        digitalWrite(PB14, HIGH);
+        break;
+    case 6:
+        digitalWrite(PB4, LOW);
+        digitalWrite(PB3, HIGH);
+        digitalWrite(PA15, HIGH);
+        digitalWrite(PA8, LOW);
+        digitalWrite(PB15, HIGH);
+        digitalWrite(PB14, HIGH);
+        break;
+    case 7:
+        digitalWrite(PB4, HIGH);
+        digitalWrite(PB3, HIGH);
+        digitalWrite(PA15, HIGH);
+        digitalWrite(PA8, HIGH);
+        digitalWrite(PB15, HIGH);
+        digitalWrite(PB14, HIGH);
+        break;
+    }
+}
